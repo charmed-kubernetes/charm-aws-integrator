fec5992e4bf495b924bc5413a41169a69b7fe3ef
Adam Dyess <adam.dyess@canonical.com>
2025-01-24 13:28:37 -0600

    Review comments

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,7 +1,7 @@
-terraform {
-  # Copyright 2025 Canonical Ltd.
-  # See LICENSE file for licensing details.
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
 
+terraform {
   required_version = ">= 1.6"
   required_providers {
     juju = {
